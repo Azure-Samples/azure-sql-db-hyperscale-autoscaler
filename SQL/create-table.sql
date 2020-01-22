a32a37fb63d7795451d3f44ce039c8ede912c171
Davide Mauri <damauri@microsoft.com>
2020-01-22 13:37:05 -0800

    added sql scripts to create objects and run load tests

---
+++ SQL/create-table.sql
@@ -0,0 +1,27 @@
+﻿DROP TABLE IF EXISTS [dbo].[AutoscalerMonitor]
+GO
+
+CREATE TABLE [dbo].[AutoscalerMonitor]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY CLUSTERED,
+	[InsertedAt] DATETIME2(7) NOT NULL DEFAULT (SYSUTCDATETIME()),
+	[CurrentSLO] NVARCHAR(100) NOT NULL DEFAULT (CAST(DATABASEPROPERTYEX(DB_NAME(DB_ID()),'ServiceObjective') AS NVARCHAR(100))),
+	[RequestedSLO] NVARCHAR(100) NULL,
+	[UsageInfo] NVARCHAR(MAX) NULL CHECK(ISJSON([UsageInfo])=1)
+)
+GO
+
+DROP TABLE IF EXISTS [dbo].[Numbers]
+GO
+
+SELECT TOP (1000000)
+	ROW_NUMBER() OVER (ORDER BY A.[object_id]) AS Number,
+	RAND(CHECKSUM(NEWID())) AS Random
+INTO
+	[dbo].[Numbers]
+FROM
+	sys.[all_columns] a, sys.[all_columns] b
+GO
+
+CREATE CLUSTERED INDEX ixc ON dbo.[Numbers](Number)
+GO
---
+++ SQL/load-test.sql
@@ -0,0 +1,20 @@
+﻿WITH cte AS
+(
+	SELECT
+		CASE WHEN (Number % 2) = 1 THEN 1 ELSE 0 END AS GroupId,
+		Number,
+		Random
+	FROM
+		dbo.[Numbers]
+	WHERE
+		[Number] BETWEEN 1 AND 300000
+)
+SELECT
+	GroupId,
+	COUNT(*),
+	AVG(Random),
+	STDEV(Random)
+FROM
+	cte
+GROUP BY
+	GroupId
