6a471f7740f369a792eeaf6e1e2e98112516ea9c
Davide Mauri <damauri@microsoft.com>
2020-01-21 16:50:21 -0800

    updated to azure functions 3.0

--- AutoScaler.cs
+++ AutoScaler.cs
@@ -12,19 +12,21 @@ namespace Azure.SQL.DB.Hyperscale.Tools
 {
     public class HyperScaleTier
     {
-        private string Name = "hs";
+        private readonly string Name = "hs";
         public int Generation = 5;
         public int Cores = 2;
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return $"{Name}_gen{Generation}_{Cores}";
         }
 
-        public override bool Equals(object obj) {            
-            if (Object.ReferenceEquals(obj, null))
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
                 return false;
- 
-            if (Object.ReferenceEquals(this, obj))
+
+            if (object.ReferenceEquals(this, obj))
                 return true;
 
             if (this.GetType() != obj.GetType())
@@ -32,22 +34,22 @@ namespace Azure.SQL.DB.Hyperscale.Tools
 
             return this.ToString() == obj.ToString();
         }
-        
-        public override int GetHashCode() {
+
+        public override int GetHashCode()
+        {
             return this.ToString().GetHashCode();
         }
 
         public static bool operator == (HyperScaleTier lhs, HyperScaleTier rhs)
         {
-            if (Object.ReferenceEquals(lhs, null))
+            if (lhs is null)
             {
-                if (Object.ReferenceEquals(rhs, null))
-                {
+                if (rhs is null)
                     return true;
-                }
 
                 return false;
             }
+
             return lhs.Equals(rhs);
         }
 
@@ -59,16 +61,16 @@ namespace Azure.SQL.DB.Hyperscale.Tools
         public static HyperScaleTier Parse(string tierName)
         {
             var curName = tierName.ToLower();
-            var parts =  curName.Split('_');
-            
+            var parts = curName.Split('_');
+
             if (parts[0] != "hs") throw new ArgumentException($"'{tierName}' is not an Hyperscale Tier");
 
             var result = new HyperScaleTier();
             result.Generation = int.Parse(parts[1].Replace("gen", string.Empty));
             result.Cores = int.Parse(parts[2]);
-            
+
             return result;
-        }       
+        }
     }
 
     public class UsageInfo
@@ -86,7 +88,8 @@ namespace Azure.SQL.DB.Hyperscale.Tools
         Previous
     }
 
-    public class AutoScalerConfiguration {
+    public class AutoScalerConfiguration
+    {
         public int vCoreMin = int.Parse(Environment.GetEnvironmentVariable("vCoreMin"));
         public int vCoreMax = int.Parse(Environment.GetEnvironmentVariable("vCoreMax"));
         public decimal HighThreshold = decimal.Parse(Environment.GetEnvironmentVariable("HighThreshold"));
@@ -95,10 +98,10 @@ namespace Azure.SQL.DB.Hyperscale.Tools
 
     public static class AutoScaler
     {
-        public static readonly List<String> GEN4 = new List<String>() { "hs_gen4_1", "hs_gen4_2", "hs_gen4_3", "hs_gen4_4", "hs_gen4_5", "hs_gen4_6", "hs_gen4_7", "hs_gen4_8", "hs_gen4_9", "hs_gen4_10","hs_gen4_16", "hs_gen4_24" };
+        public static readonly List<String> GEN4 = new List<String>() { "hs_gen4_1", "hs_gen4_2", "hs_gen4_3", "hs_gen4_4", "hs_gen4_5", "hs_gen4_6", "hs_gen4_7", "hs_gen4_8", "hs_gen4_9", "hs_gen4_10", "hs_gen4_16", "hs_gen4_24" };
 
         public static readonly List<String> GEN5 = new List<String>() { "hs_gen5_2", "hs_gen5_4", "hs_gen5_6", "hs_gen5_8", "hs_gen5_10", "hs_gen5_12", "hs_gen5_14", "hs_gen5_16", "hs_gen5_18", "hs_gen5_20", "hs_gen5_24", "hs_gen5_32", "hs_gen5_40", "hs_gen5_80" };
-        
+
         public static Dictionary<int, List<String>> HyperscaleSLOs = new Dictionary<int, List<String>>();
 
         static AutoScaler()
@@ -119,7 +122,7 @@ namespace Azure.SQL.DB.Hyperscale.Tools
             {
                 // TODO: make sure at least 4 data points are collected (1 minute)
                 // otherwise autoscaler will always bounce back to lower SLO after increasing it
-                
+
                 var result = conn.QuerySingleOrDefault<UsageInfo>(@"
                     select top 1
                         [end_time] as TimeStamp, 
@@ -131,9 +134,10 @@ namespace Azure.SQL.DB.Hyperscale.Tools
                         sys.dm_db_resource_stats
                     order by 
                         end_time desc 
-                ");                
+                ");
 
-                if (result == null) {
+                if (result == null)
+                {
                     log.LogInformation("No information received from server.");
                     return;
                 }
@@ -152,23 +156,24 @@ namespace Azure.SQL.DB.Hyperscale.Tools
 
                 // Scale Up
                 if (result.MovingAvgCpuPercent > autoscalerConfig.HighThreshold)
-                {                    
+                {
                     var targetSlo = GetServiceObjective(result.ServiceObjective, SearchDirection.Next);
                     if (targetSlo != null && currentSlo.Cores < autoscalerConfig.vCoreMax && currentSlo != targetSlo)
                     {
                         log.LogInformation($"HIGH threshold reached: scaling up to {targetSlo}");
                         conn.Execute($"alter database [{databaseName}] modify (service_objective = '{targetSlo}')");
-                    }                        
+                    }
                 }
 
                 // Scale Down
                 if (result.MovingAvgCpuPercent < autoscalerConfig.LowThreshold)
-                {                                        
+                {
                     var targetSlo = GetServiceObjective(result.ServiceObjective, SearchDirection.Previous);
-                    if (targetSlo != null && currentSlo.Cores > autoscalerConfig.vCoreMin && currentSlo != targetSlo) {                        
+                    if (targetSlo != null && currentSlo.Cores > autoscalerConfig.vCoreMin && currentSlo != targetSlo)
+                    {
                         log.LogInformation($"LOW threshold reached: scaling down to {targetSlo}");
                         conn.Execute($"alter database [{databaseName}] modify (service_objective = '{targetSlo}')");
-                    }                        
+                    }
                 }
             }
         }
@@ -181,17 +186,11 @@ namespace Azure.SQL.DB.Hyperscale.Tools
             var availableSlos = HyperscaleSLOs[curHS.Generation];
             var index = availableSlos.IndexOf(curHS.ToString());
 
-            if (direction == SearchDirection.Next)
-            {
-                if (index < availableSlos.Count) 
-                    newHS = HyperScaleTier.Parse(availableSlos[index+1]);
-            }
-            
-            if (direction == SearchDirection.Previous)
-            {
-                if (index > 0) 
-                    newHS = HyperScaleTier.Parse(availableSlos[index-1]);                    
-            }
+            if (direction == SearchDirection.Next && index < availableSlos.Count)
+                    newHS = HyperScaleTier.Parse(availableSlos[index + 1]);
+
+            if (direction == SearchDirection.Previous && index > 0)
+                    newHS = HyperScaleTier.Parse(availableSlos[index - 1]);
 
             return newHS;
         }
